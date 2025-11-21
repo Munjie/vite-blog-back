74b2395a984d77e3384ef7f6e53b833c6535875a
Munjie <892790756@qq.com>
2025-11-21 14:06:08 +0800

    feat# 更新请求地址,dev 去掉api

--- src/api/http.ts
+++ src/api/http.ts
@@ -12,10 +12,10 @@ export interface Result<T = any> {
     message: string;
     data: T;
 }
-
+const isDevelopment = import.meta.env.MODE === 'development'
 // 2. 创建 axios 实例
 const service: AxiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_APP_API_URL,
+    baseURL: isDevelopment ? '' : import.meta.env.VITE_APP_API_URL,
     timeout: 10000, // 请求超时时间
     headers: {
         'Content-Type': 'application/json;charset=utf-8',
