8770d7f6542ed900eae47a9a93cbd72b65e50c42
Munjie <892790756@qq.com>
2025-11-21 18:46:56 +0800

    feat# 更新登录页

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,8 +1,55 @@
+<template>
+    <div class="login-bg">
+        <div class="login-container">
+            <div class="login-header">
+                <img class="logo mr10" src="../../assets/logo.svg" alt="" />
+                <div class="login-title">后台管理系统</div>
+            </div>
+            <el-form :model="form" :rules="rules" ref="loginFormRef" size="large">
+                <el-form-item prop="username">
+                    <el-input v-model="form.username" placeholder="用户名">
+                        <template #prepend>
+                            <el-icon>
+                                <User />
+                            </el-icon>
+                        </template>
+                    </el-input>
+                </el-form-item>
+                <el-form-item prop="password">
+                    <el-input
+                        type="password"
+                        placeholder="密码"
+                        v-model="form.password"
+                        @keyup.enter="handleLogin"
+                    >
+                        <template #prepend>
+                            <el-icon>
+                                <Lock />
+                            </el-icon>
+                        </template>
+                    </el-input>
+                </el-form-item>
+                <div class="pwd-tips">
+                    <el-checkbox class="pwd-checkbox" v-model="checked" label="记住密码" />
+                    <el-link type="primary" @click="router.push('/reset-pwd')">忘记密码</el-link>
+                </div>
+                <el-button class="login-btn" type="primary" size="large" @click="handleLogin">登录</el-button>
+                <p class="login-text">
+                    没有账号？<el-link type="primary" @click="router.push('/register')">立即注册</el-link>
+                </p>
+            </el-form>
+        </div>
+    </div>
+</template>
+
 <script setup lang="ts">
 import {ref, reactive} from 'vue';
 import {useRouter} from 'vue-router';
 import type {FormRules} from 'element-plus';
 import {login} from '../../api/login.ts'
+
+const lgStr = localStorage.getItem('login-param');
+const checked = ref(lgStr ? true : false);
 
 const router = useRouter();
 const loading = ref(false);
@@ -39,27 +86,69 @@ const handleLogin = async () => {
 };
 </script>
 
-<template>
-    <div class="login-container">
-        <el-card class="box-card">
-            <template #header>
-                <span>登录</span>
-            </template>
-            <el-form :model="form" :rules="rules" ref="loginFormRef" label-width="100px" class="demo-loginForm">
-                <el-form-item label="用户名" prop="username">
-                    <el-input v-model="form.username"></el-input>
-                </el-form-item>
-                <el-form-item label="密码" prop="password">
-                    <el-input type="password" v-model="form.password" show-password></el-input>
-                </el-form-item>
-                <el-form-item>
-                    <el-button type="primary" @click="handleLogin">登录</el-button>
-                </el-form-item>
-            </el-form>
-        </el-card>
-    </div>
-</template>
-
 <style scoped>
+.login-bg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100vh;
+    background: url(../../assets/login-bg.jpg) center/cover no-repeat;
+}
 
+.login-header {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 40px;
+}
+
+.logo {
+    width: 35px;
+}
+
+.login-title {
+    font-size: 22px;
+    color: #333;
+    font-weight: bold;
+}
+
+.login-container {
+    width: 450px;
+    border-radius: 5px;
+    background: #fff;
+    padding: 40px 50px 50px;
+    box-sizing: border-box;
+}
+
+.pwd-tips {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 14px;
+    margin: -10px 0 10px;
+    color: #787878;
+}
+
+.pwd-checkbox {
+    height: auto;
+}
+
+.login-btn {
+    display: block;
+    width: 100%;
+}
+
+.login-tips {
+    font-size: 12px;
+    color: #999;
+}
+
+.login-text {
+    display: flex;
+    align-items: center;
+    margin-top: 20px;
+    font-size: 14px;
+    color: #787878;
+}
 </style>
