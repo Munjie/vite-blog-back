a330b99d4f512ce5329662f9209da4e564cbe17c
Munjie <892790756@qq.com>
2025-12-05 16:35:16 +0800

    feat# 更新首页 箱线图

--- src/views/chart/options.ts
+++ src/views/chart/options.ts
@@ -1,3 +1,71 @@
+export const boxOptions = {
+
+        title: [
+            {
+                text: "成绩分布箱体图",
+                left: "center",
+                textStyle: {
+                    fontSize: 20,
+                    color: "green",
+                },
+            },
+        ],
+        dataset: [
+            {
+                // prettier-ignore
+                source: [
+                    [850, 740, 900, 1070, 930, 850, 950, 980, 980, 880, 1000, 980, 930, 650, 760, 810, 1000, 1000, 960, 960],
+                    [960, 940, 960, 940, 880, 800, 850, 880, 900, 840, 830, 790, 810, 880, 880, 830, 800, 790, 760, 800],
+                    [880, 880, 880, 860, 720, 720, 620, 860, 970, 950, 880, 910, 850, 870, 840, 840, 850, 840, 840, 840],
+                    [890, 810, 810, 820, 800, 770, 760, 740, 750, 760, 910, 920, 890, 860, 880, 720, 840, 850, 850, 780],
+                    [890, 840, 780, 810, 760, 810, 790, 810, 820, 850, 870, 870, 810, 740, 810, 940, 950, 800, 810, 870]
+                ]
+            },
+            {
+                transform: {
+                    type: 'boxplot',
+                    config: { itemNameFormatter: 'expr {value}' }
+                }
+            },
+            {
+                fromDatasetIndex: 1,
+                fromTransformResult: 1
+            }
+        ],
+    tooltip: {
+        trigger: "item", //触发类型,数据项图形触发，主要在散点图，饼图等无类目轴的图表中使用。
+        axisPointer: {
+            //指示器类型。
+            type: "shadow",
+        },
+    },
+    grid: {
+        //直角坐标系网格。
+        //show: true,//default: false
+        left: "10%",
+        right: "10%",
+        bottom: "15%",
+        //borderWidth: 1,
+        //borderColor: '#000',
+    },
+    xAxis: {
+        type: "category", //'category' 类目轴，适用于离散的类目数据，为该类型时必须通过 data 设置类目数据。
+        show: false,
+    },
+        yAxis: {},
+        series: [
+            {
+                name: 'boxplot',
+                type: 'boxplot',
+                datasetIndex: 1
+            },
+            {
+                name: 'outlier',
+                type: 'scatter',
+                datasetIndex: 2
+            }
+        ]
+};
 
 export const barOptions = {
     xAxis: {
