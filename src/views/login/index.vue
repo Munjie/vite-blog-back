690ffbfe50c297c214a262548a9a4c4a99b55ea9
Munjie <892790756@qq.com>
2025-12-10 13:15:21 +0800

    feat# 更新image

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,7 +2,7 @@
     <div class="login-bg">
         <div class="login-container">
             <div class="login-header">
-                <img class="logo mr10" src="../../assets/logo.svg" alt="" />
+                <img class="logo mr10" src="../../assets/img/login-index.svg" alt="" />
                 <div class="login-title">AI Cloud System</div>
             </div>
             <el-form :model="form" :rules="rules" ref="loginFormRef" size="large">
@@ -108,7 +108,7 @@ tabs.clearTabs();
     justify-content: center;
     width: 100%;
     height: 100vh;
-    background: url(../../assets/login-bg.jpg) center/cover no-repeat;
+    background: url(../../assets/img/bg_login.jpg) center/cover no-repeat;
 }
 
 .login-header {
