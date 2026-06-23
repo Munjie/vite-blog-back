2b003d4d71a43603611cdd9e92281a5079091e1c
Munjie <892790756@qq.com>
2026-06-23 09:55:20 +0800

    feat# 更新 article

--- src/api/comment.ts
+++ src/api/comment.ts
@@ -5,20 +5,20 @@ import http from "./http.ts";
 
 export const getAllComment = (data?: object) => {
     return http.post(
-        '/api/back/page-all-comment',
+        '/api/article/page-all-comment',
         data
     );
 };
 
 
 export function deleteComment(id: number) {
-    return http.delete<void>('/api/back/delete-comment', { id: id });
+    return http.delete<void>('/api/article/delete-comment', { id: id });
 }
 
 
 export const updateCommentStatus = (data?: object) => {
     return http.post(
-        '/api/back/update-comment-status',
+        '/api/article/update-comment-status',
         data
     );
 };
